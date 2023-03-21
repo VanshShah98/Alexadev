38d32fdabfc52852d27890f2763fa06e607d6571
VanshShah98 <vanshshah0908@gmail.com>
2023-03-21 23:28:57 +0530

    sixth commit

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
  app.use(express.json())
 
 app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
+//app.use('/api/notes', require('./routes/notes'))
 app.listen(port, () => {
   console.log(`Server is running on the port ${port}.`);
 });
--- Backend/middleware/fetchuser.js
+++ Backend/middleware/fetchuser.js
@@ -1,17 +1,21 @@
 var jwt = require('jsonwebtoken');
-const JWT_SECRET='Vansh$123';
-const fetchuser=(req,res,next) =>{
-    const token=req.header('auth-token');
-    if(!token){
-        res.status(401).send("valid token pls")
-    }
-    try{
-        const string=jwt.verify(token,JWT_SECRET);
-        req.user.id=data.user;
-        next()
-    }catch(error){
-        res.status(401).send("valid token pls")
-    }
-} 
+const JWT_SECRET = 'Harryisagoodb$oy';
 
-module.exports=fetchuser;
+const fetchuser = (req, res, next) => {
+    // Get the user from the jwt token and add id to req object
+    const token = req.header('auth-token');
+    if (!token) {
+        res.status(401).send({ error: "Please authenticate using a valid token" })
+    }
+    try {
+        const data = jwt.verify(token, JWT_SECRET);
+        req.user = data.user;
+        next();
+    } catch (error) {
+        res.status(401).send({ error: "Please authenticate using a valid token" })
+    }
+
+}
+
+
+module.exports = fetchuser;
